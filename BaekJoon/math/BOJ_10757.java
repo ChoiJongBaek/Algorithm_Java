30a433c0096ce017d55c1cf8e9be5e6ec4350ea6
ChoiJongBaek <59366717+ChoiJongBaek@users.noreply.github.com>
2021-03-04 12:10:11 +0900

    BOJ_10757

---
+++ BaekJoon/math/BOJ_10757.java
@@ -0,0 +1,17 @@
+package BaekJoon.math;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ_10757 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger b1 = sc.nextBigInteger();
+		BigInteger b2 = sc.nextBigInteger();
+		
+		System.out.println(b1.add(b2));
+		
+	}
+}
