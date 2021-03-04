4431ed904408aca01f44d7d9b103d9cd79f29d75
ChoiJongBaek <59366717+ChoiJongBaek@users.noreply.github.com>
2021-03-04 13:33:25 +0900

    21.03.04 BOJ_10828

--- BaekJoon/stack/BOJ_10828.java
+++ BaekJoon/stack/BOJ_10828.java
@@ -45,4 +45,43 @@ public class BOJ_10828 {
 		}
 		System.out.println(sb);
 	}
+	
+	/*
+	 * 처음 방법
+	 public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		Stack stack = new Stack();
+		
+		for(int i=0;i<num;i++) {
+			String act = br.readLine();
+			StringTokenizer st = new StringTokenizer(act);
+			switch(st.nextToken()) {
+				case "push" : stack.push(st.nextToken());
+					break;
+				case "pop" : if(stack.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(stack.pop());
+				}
+					break;
+				case "size" : System.out.println(stack.size());
+					break;
+				case "empty" : if(stack.isEmpty()) {
+					System.out.println(1);
+				} else {
+					System.out.println(0);
+				}
+					break;
+				case "top" : if(stack.isEmpty()) {
+					System.out.println(-1);
+				} else {
+					System.out.println(stack.peek());
+				}
+					break;
+			}
+		}
+	}
+	 */
 }
